08e94d77f39e140ded7529e1cc11380e479883ad
ConnorCleveland2002 <connorcleveland2002@gmail.com>
2021-08-24 09:57:45 -0400

    Added everything but delte and update comments, and updating posts

---
+++ public/js/comments.js
@@ -0,0 +1,29 @@
+const newFormHandler = async (event) => {
+  event.preventDefault();
+
+  const comment = document.querySelector(".new-comment").value.trim();
+  const post_id = document
+    .querySelector(".btn-comment")
+    .getAttribute("data-id");
+
+  console.log(event.target.getAttribute("data-id"));
+  if (comment) {
+    const response = await fetch(`/api/comments`, {
+      method: "POST",
+      body: JSON.stringify({ comment, post_id }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace(`/post/${post_id}`);
+    } else {
+      alert("Failed to create a new comment");
+    }
+  }
+};
+
+document
+  .querySelector(".btn-comment")
+  .addEventListener("click", newFormHandler);
